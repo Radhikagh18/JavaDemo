da3331b2d133e5d19cdee42437c01ef696523476
Radhika <radhika.kondavaram0@walmart.com>
2023-09-08 11:50:54 +0530

    second change

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ class Hello
 public static void main(String[] args)
 {
 System.out.println("Welcome to java Build");
+System.out.println("Am interested to learn Jenkins");
+System.out.println(45+55);
 }
 }
